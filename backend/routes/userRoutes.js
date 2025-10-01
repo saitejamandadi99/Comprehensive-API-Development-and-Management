b578e3f2a4c2217ea8345b4248ce7b5bd344b11e
saitejamandadi99 <saitejamandadi99>
2025-10-01 20:56:05 +0530

    added the middleware to the routing

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express'); 
 const router = express.Router(); 
+const authMiddleware = require('../middleware/authMiddleware');
 
 const {getAllUsers, getUserById, updateUserById, deleteUserById} = require('../controllers/userController');
 
-router.get('/', getAllUsers);
-router.get('/:id', getUserById);
-router.put('/:id', updateUserById);
-router.delete('/:id', deleteUserById);
+router.get('/',authMiddleware, getAllUsers);
+router.get('/:id',authMiddleware, getUserById);
+router.put('/:id',authMiddleware, updateUserById);
+router.delete('/:id',authMiddleware, deleteUserById);
 module.exports = router;
