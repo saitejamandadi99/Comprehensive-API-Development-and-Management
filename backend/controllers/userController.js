2bc03d77c78afdab074992ec9a2b305b74f35302
saitejamandadi99 <saitejamandadi99>
2025-10-01 20:18:03 +0530

    changed status codes to 200 (ok) from 201 (created)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -45,7 +45,7 @@ const updateUserById = async (req, res)=>{
         if(!user){
             return res.status(404).json({message: 'User not found'})
         }
-        return res.status(201).json({message: 'User updated successfully', user})        
+        return res.status(200).json({message: 'User updated successfully', user})        
     } catch (error) {
         console.error('Error in updateUserById:', error.message);
         return res.status(500).json({message: error.message})
@@ -61,7 +61,7 @@ const deleteUserById = async (req, res)=>{
         if(!user){
             return res.status(404).json({message: 'User not found'})
         }
-        return res.status(201).json({message: 'User deleted successfully', user})
+        return res.status(200).json({message: 'User deleted successfully', user})
         
     } catch (error) {
         console.error('Error in deleteUserById:', error.message);
