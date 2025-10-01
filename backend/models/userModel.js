d9d664897f3e56c99557b2aa6e402e3087b0c9f2
saitejamandadi99 <saitejamandadi99>
2025-10-01 16:41:13 +0530

    created a new user model to store the user details

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose; 
+const userSchema = new Schema({
+    name:{
+        type:String, 
+        required:true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"]
+    },
+    phone:{
+        type:String, 
+        required:true,
+        unique:true,
+        match: [/^\d{10}$/, "Please enter a valid 10-digit phone number"]
+    },
+    password:{
+        type:String, 
+        required:true
+    },
+    },{timestamps:true})
+
+module.exports = mongoose.model('User', userSchema);
